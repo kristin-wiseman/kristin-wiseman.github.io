8ae5efed0484ff5e8740720e48e9efd615514c31
kristin-wiseman <kristin.w.dev@gmail.com>
2023-02-09 19:14:01 -0600

    mui theme colors experiment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import './App.css';
 
+// MUI Theme & Styling
+import { ThemeProvider, createTheme} from '@mui/material/styles';
+
+// MUI Components:
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+
+// MUI Icons:
 import MenuIcon from '@mui/icons-material/Menu';
+
+const kwTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#060039'
+    },
+    secondary: {
+      main: '#918ea5'
+    },
+    neutral: {
+      main: '#929094'
+    },
+    error: {
+      main: '#ba1a1a'
+    },
+    contrastThreshold: 4.5
+  }
+});
 
 function NavBar(props) {
   return (
     <Box>
       <AppBar>
-        <IconButton>
-          <MenuIcon/>
+        <IconButton aria-label="menu" size="large">
+          <MenuIcon fontSize="inherit" />
         </IconButton>
       </AppBar>
     </Box>
@@ -27,18 +51,18 @@ class App extends React.Component {
 
   render() {
   return (
-    <>
+    <ThemeProvider theme={kwTheme}>
       <header>
         <NavBar></NavBar>
       </header>
-      <div>
+      <Box>
         <p>More Coming Soon!</p>
         <p>
           For now, please find me on&nbsp;
           <a href='https://linkedin.com/in/kristinwiseman' rel="noreferrer" target="_blank">LinkedIn</a>
         </p>
-      </div>
-    </>
+      </Box>
+    </ThemeProvider>
   );
   }
 }
