73f975018d9b9146c6b1e0ddeec14a5a38ba36b4
kristin-wiseman <kristin.w.dev@gmail.com>
2023-02-11 08:30:04 -0600

    Trying to fix default export issue

--- src/PrimaryIconButton.js
+++ src/PrimaryIconButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
-import styled from '@mui/material/styles';
+import {styled} from '@mui/material/styles';
 
 const PrimaryIconButton = styled(IconButton)(({theme}) => ({
     color: theme.palette.primary.contrastText,
