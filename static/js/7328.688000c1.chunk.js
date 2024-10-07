22fa42ad99058c1575ff3e49e4e311e825918c90
Kristin <88798340+kristin-wiseman@users.noreply.github.com>
2024-10-06 20:38:15 -0500

    Upgrade to mui 6, npm audit fixes, minor aesthetic improvements.

---
+++ static/js/7328.688000c1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkristin_wiseman_github_io=self.webpackChunkkristin_wiseman_github_io||[]).push([[7328],{7328:(i,s,e)=>{e.r(s),e.d(s,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=7328.688000c1.chunk.js.map
